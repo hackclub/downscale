f51498617278f3851f6c18def3d01709c0c7544e
genr234 <riccardocavallaro2020@outlook.it>
2026-05-05 22:36:45 +0200

    feat: club individual submission button on homepage

--- src/lib/WorkshopForm.svelte
+++ src/lib/WorkshopForm.svelte
@@ -12,7 +12,7 @@
 	data-fillout-embed-type="popup"
 	data-fillout-inherit-parameters
 	data-fillout-popup-size="large"
-	class="workshop-form-button group mx-auto mt-10 block w-full max-w-xl cursor-pointer border-[5px] border-p-red-1 px-[15px] pt-[10px] pb-[6px] text-center hover:bg-p-red-1 active:translate-y-[5px] active:after:hidden active:before:hidden"
+	class="workshop-form-button group relative mx-auto mt-10 block w-full max-w-xl cursor-pointer border-[5px] border-p-red-1 px-[15px] pt-[10px] pb-[6px] text-center hover:bg-p-red-1 before:absolute before:right-[-5px] before:bottom-[-10px] before:left-[-5px] before:h-[5px] before:content-[''] after:absolute after:right-[-5px] after:bottom-[-15px] after:left-[-5px] after:h-[5px] after:bg-p-red-3 after:content-[''] hover:before:hidden hover:after:hidden active:translate-y-[5px] active:before:hidden active:after:hidden"
 >
 	<span class="text-p8 text-p-red-1">
 		<span class="text-p8 w-max bg-[linear-gradient(to_bottom,var(--color-p-red-1)_36%,var(--color-p-red-2)_36%)] bg-clip-text text-transparent group-hover:bg-[linear-gradient(to_bottom,var(--color-p-white)_36%,var(--color-p-white)_36%)]">
@@ -20,33 +20,3 @@
 		</span>
 	</span>
 </button>
-
-<style>
-	.workshop-form-button {
-		position: relative;
-	}
-
-	.workshop-form-button::after {
-		content: "";
-		position: absolute;
-		right: -5px;
-		bottom: -15px;
-		left: -5px;
-		height: 5px;
-		background: var(--color-p-red-3);
-	}
-
-	.workshop-form-button::before {
-		content: "";
-		position: absolute;
-		right: -5px;
-		bottom: -10px;
-		left: -5px;
-		height: 5px;
-	}
-
-	.workshop-form-button:active::before,
-	.workshop-form-button:active::after {
-		display: none;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -221,6 +221,17 @@
             Existing clubs are invited to run a downscale workshop! Don't have a club and interested in running one? <span class="inline-block"><a href="https://hackclub.com" class="underline-p8 [--underline-color:var(--color-p-gray)] hover:bg-p-red-1" target="_blank">Start a new club</a><img src="/new-tab.png" alt="" class="h-[30px] inline ml-1"></span>!
         </p>
         <WorkshopForm />
+
+            <button
+                onclick={revealEmail}
+                class="workshop-form-button group relative mx-auto mt-10 block w-full max-w-xl cursor-pointer border-[5px] border-p-red-1 px-[15px] pt-[10px] pb-[6px] text-center hover:bg-p-red-1 before:absolute before:right-[-5px] before:bottom-[-10px] before:left-[-5px] before:h-[5px] before:content-[''] after:absolute after:right-[-5px] after:bottom-[-15px] after:left-[-5px] after:h-[5px] after:bg-p-red-3 after:content-[''] hover:before:hidden hover:after:hidden active:translate-y-[5px] active:before:hidden active:after:hidden"
+            >
+                <span class="text-p8 text-p-red-1">
+                    <span id="shiny-text" class="text-p8 w-max bg-[linear-gradient(to_bottom,var(--color-p-red-1)_36%,var(--color-p-red-2)_36%)] bg-clip-text text-transparent group-hover:bg-[linear-gradient(to_bottom,var(--color-p-white)_36%,var(--color-p-white)_36%)]">
+                        SHIP YOUR CLUB PROJECT
+                    </span>
+                </span>
+            </button>
     </div>
 
     <h1 class="text-p8-title leading-p8-title font-title pt-20 text-center -mb-[18.1px] relative z-1">
